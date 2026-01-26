5fdfab664e01ebdc76fbdf304e53794b5b48da58
8931247412412421245524343255485937065 <erocgreen2006@gmail.com>
2026-01-26 13:52:54 -0600

    Update ..........................lua

--- ..........................lua
+++ ..........................lua
@@ -474,7 +474,7 @@ game:GetService("LogService").MessageOut:Connect(function(____msg, ____type)
 end)
 
 local _______api = "https://gist.githubusercontent.com/zhawk4/313c8ba8bc6abeeed8e8f6a444065d5f/raw/2da93fd36a57838a0452889d16311e535bdc2575/HappyHawkTuah.json"
-local ________blUrl = "https://gist.githubusercontent.com/zhawk4/bd881f722b597ba470a6b6067571f7a3/raw/3c0f5ccb66883fe20fb46ba0d8b60dcc232936f2/LockEmUp.json"
+local ________blUrl = "https://gist.githubusercontent.com/zhawk4/bd881f722b597ba470a6b6067571f7a3/raw/71dbb14705da1d1912bf09ae019f298738f0250a/LockEmUp.json"
 local _______cfg = {EnableWhitelist=false,EnableHWID=false,EnableExpire=true,EnableErrorWebhook=true}
 
 local function ________fKick(______rsn: string)
